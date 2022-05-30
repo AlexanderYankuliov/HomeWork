79ae103c1bf5b29958a13583a267173f29451273
Alexander Yankuliov <6024507@gmail.com>
2022-05-30 13:25:15 +0300

    no message

--- HomeWork/HomeWork/ViewController.swift
+++ HomeWork/HomeWork/ViewController.swift
@@ -5,10 +5,6 @@
 //  Created by Alexander Yankuliov on 26.05.22.
 //
 //
-//  Задание:
-//
-//  Игрушка для котиков - появление разноцветного кружочка, по тапу исчезает и появляется в новом месте (кодом).
-
 
 import UIKit
 
@@ -16,7 +12,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
     }
     
 }
